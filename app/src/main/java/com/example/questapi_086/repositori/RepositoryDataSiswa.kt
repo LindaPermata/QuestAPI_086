3d80bfadadc04cdeafacb176589a23231fda7529
LindaPermata <permatalinda531@gmail.com>
2025-12-18 12:10:29 +0700

    menambahkan implementasi repository jaringan data Siswa

--- app/src/main/java/com/example/questapi_086/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_086/repositori/RepositoryDataSiswa.kt
@@ -1,9 +1,17 @@
 package com.example.questapi_086.repositori
 
+import com.example.questapi_086.apiservice.ServiceApiSiswa
 import com.example.questapi_086.modeldata.DataSiswa
 
-interface RepositoryDataSiswa {
-    suspend fun getDataSiswa() : List<DataSiswa>
+interface RepositoryDataSiswa{
+    suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
 }
 
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+):RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+
+}
