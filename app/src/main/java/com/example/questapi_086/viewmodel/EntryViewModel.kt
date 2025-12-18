b4391edeef98e44a832e0f1f3b0ce835e49c2a85
LindaPermata <permatalinda531@gmail.com>
2025-12-18 12:56:03 +0700

    menambahkan EntryViewModel untuk input dan validasi data Siswa

--- app/src/main/java/com/example/questapi_086/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/questapi_086/viewmodel/EntryViewModel.kt
@@ -1,4 +1,40 @@
-package com.example.questapi_086.viewmodel;
+package com.example.questapi_086.viewmodel
 
-public class EntryViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.questapi_086.modeldata.DetailSiswa
+import com.example.questapi_086.modeldata.UIStateSiswa
+import com.example.questapi_086.modeldata.toDataSiswa
+import com.example.questapi_086.repositori.RepositoryDataSiswa
+import retrofit2.Response
+
+class EntryViewModel (private val repositoryDataSiswa: RepositoryDataSiswa) :
+    ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa):
+            Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+
+    }
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            val sip: Response<Void> = repositoryDataSiswa
+                .postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+            if (sip.isSuccessful){
+                println("Sukses tambah data : ${sip.message()}")
+            }else {
+                println("Gagal tambah data : ${sip.errorBody()}")
+            }
+        }
+    }
 }
