ae1d1436322a06e5823c89f4f70c404a5778f009
LindaPermata <permatalinda531@gmail.com>
2025-12-18 12:38:34 +0700

    memperbaiki error

--- app/src/main/java/com/example/questapi_086/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/questapi_086/viewmodel/HomeViewModel.kt
@@ -1,42 +1,40 @@
 package com.example.questapi_086.viewmodel
 
-import android.net.http.HttpException
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.questapi_086.modeldata.DataSiswa
 import com.example.questapi_086.repositori.RepositoryDataSiswa
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
 import java.io.IOException
 
 
-
 sealed interface StatusUiSiswa {
-    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
+    data class Success(val listSiswa: List<DataSiswa>) : StatusUiSiswa
     object Error : StatusUiSiswa
     object Loading : StatusUiSiswa
 }
 
-class HomeViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
-        ViewModel() {
-            var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
-                private set
-            init{
-                loadSiswa()
-            }
 
-    fun loadSiswa() {
+class HomeViewModel (private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel(){
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+    init {
+        loadSiswa()
+    }
+    private fun loadSiswa() {
         viewModelScope.launch {
             listSiswa = StatusUiSiswa.Loading
             listSiswa = try {
-                StatusUiSiswa.Success(repositoryDataSiswa
-                    .getDataSiswa())
-            }catch (e: IOException){
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            }catch (e: IOException) {
                 StatusUiSiswa.Error
-            }
-            catch (e: HttpException){
+            } catch (e: HttpException){
                 StatusUiSiswa.Error
             }
         }
     }
-        }
+}
