d9097b5e78dea36e6ee58dd8a4e574163117b54f
LindaPermata <permatalinda531@gmail.com>
2025-12-25 11:25:05 +0700

    Menambahkan DetailViewModel dan inisialisasi pemanggilan data siswa

--- app/src/main/java/com/example/questapi_086/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_086/viewmodel/DetailViewModel.kt
@@ -1,9 +1,28 @@
 package com.example.questapi_086.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.questapi_086.modeldata.DataSiswa
+import com.example.questapi_086.repositori.RepositoryDataSiswa
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
 }
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+
+    private val idSiswa: Int =
+        checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
