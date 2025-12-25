830e68949767fea8a831b70b7c0d31d1fed218f2
LindaPermata <permatalinda531@gmail.com>
2025-12-25 17:35:18 +0700

    Menambahkan BodyDetailDataSiswa untuk menampilkan detail siswa

--- app/src/main/java/com/example/questapi_086/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_086/view/HalamanDetail.kt
@@ -93,3 +93,24 @@ fun DetailSiswaScreen(
     }
 }
 
+@Composable
+private fun BodyDetailDataSiswa(
+    statusUIDetail: StatusUIDetail,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+        when (statusUIDetail) {
+            is StatusUIDetail.Success -> {
+                DetailDataSiswa(
+                    siswa = statusUIDetail.satusiswa,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            else -> {}
+        }
+
