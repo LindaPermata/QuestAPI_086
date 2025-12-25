cc6d2a63339193b3c0b32f379ac00c08a303d28e
LindaPermata <permatalinda531@gmail.com>
2025-12-25 11:26:21 +0700

    Menambahkan fungsi getSatuSiswa untuk mengambil detail data siswa

--- app/src/main/java/com/example/questapi_086/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_086/viewmodel/DetailViewModel.kt
@@ -1,10 +1,16 @@
 package com.example.questapi_086.viewmodel
 
+import android.net.http.HttpException
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questapi_086.modeldata.DataSiswa
 import com.example.questapi_086.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
@@ -25,4 +31,19 @@ class DetailViewModel(
 
     init {
         getSatuSiswa()
+    }
+
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(
+                    satusiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                )
+            } catch (e: IOException) {
+                StatusUIDetail.Error
+            } catch (e: HttpException) {
+                StatusUIDetail.Error
+            }
+        }
     }
