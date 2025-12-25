61bb3de92cea20fa6ca05c62b7af5089ce5488da
LindaPermata <permatalinda531@gmail.com>
2025-12-25 11:30:14 +0700

    Menambahkan initializer DetailViewModel dengan SavedStateHandle

--- app/src/main/java/com/example/questapi_086/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi_086/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.questapi_086.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questapi_086.repositori.AplikasiDataSiswa
+import com.example.questapi_086.viewmodel.DetailViewModel
 import com.example.questapi_086.viewmodel.EntryViewModel
 import com.example.questapi_086.viewmodel.HomeViewModel
 
@@ -16,5 +18,11 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa) }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
     }
 }
