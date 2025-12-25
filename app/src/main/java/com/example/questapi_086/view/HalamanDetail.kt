c6e8d8e4dce68d38d43655fc640e128443d8fabe
LindaPermata <permatalinda531@gmail.com>
2025-12-25 17:37:29 +0700

    Menambahkan tampilan detail nama, alamat, dan telepon siswa

--- app/src/main/java/com/example/questapi_086/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_086/view/HalamanDetail.kt
@@ -145,5 +145,34 @@ fun DetailDataSiswa(
             contentColor = MaterialTheme.colorScheme.onPrimaryContainer
         )
     ) {
-
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+        ) {
+            BarisDetailData(
+                labelResID = R.string.nama,
+                itemDetail = siswa.nama,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+            BarisDetailData(
+                labelResID = R.string.alamat,
+                itemDetail = siswa.alamat,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+            BarisDetailData(
+                labelResID = R.string.telpon,
+                itemDetail = siswa.telpon,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+        }
+    }
+}
 
