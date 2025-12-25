f3a0e8278a576a4d9880f3c733b5a38789f31c9c
LindaPermata <permatalinda531@gmail.com>
2025-12-25 11:19:14 +0700

    Menambahkan inisialisasi EditViewModel dan pengambilan data siswa

--- app/src/main/java/com/example/questapi_086/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_086/viewmodel/EditViewModel.kt
@@ -1,2 +1,35 @@
 package com.example.questapi_086.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_086.modeldata.DetailSiswa
+import com.example.questapi_086.modeldata.UIStateSiswa
+import com.example.questapi_086.modeldata.toDataSiswa
+import com.example.questapi_086.modeldata.toUiStateSiswa
+import com.example.questapi_086.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int =
+        checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
+
